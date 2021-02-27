866acdb4c2a46330b97b22c00140c9cd83e08cac
ShawnMuktadir <shawn.muktadir03@gmail.com>
2021-02-28 03:00:04 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shopping_cart/cartmodel.dart';
+import 'package:flutter_shopping_cart/cartpage.dart';
+import 'package:flutter_shopping_cart/home.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+void main() => runApp(MyApp(
+    model: CartModel(),
+  ));
+
+
+class MyApp extends StatelessWidget{
+
+  final CartModel model;
+
+  const MyApp({Key key, @required this.model}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return ScopedModel<CartModel>(
+      model: model,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Shopping Cart',
+        home: HomePage(),
+        routes: {'/cart': (context) => CartPage()},
+      ),
+    );
+  }
+}
